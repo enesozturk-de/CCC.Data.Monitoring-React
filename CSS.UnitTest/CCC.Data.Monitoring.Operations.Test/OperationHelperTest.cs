a0e906bd80466b762f7060d74443651cacc0aa0a
enes.ozturk.de@gmail.com <enes.ozturk.de@gmail.com>
2021-03-12 21:59:26 +0100

    unit test added

---
+++ CSS.UnitTest/CCC.Data.Monitoring.Operations.Test/OperationHelperTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSS.UnitTest.CCC.Data.Monitoring.Operations.Test
+{
+    public class OperationHelperTest : UnitTestBase
+    {
+    }
+}
---
+++ CSS.UnitTest/UnitTestBase.cs
@@ -0,0 +1,31 @@
+﻿using Moq;
+using NSubstitute;
+using System;
+
+namespace CSS.UnitTest
+{
+    public class UnitTestBase
+    {
+        protected UnitTestBase()
+        {
+
+        }
+
+        public T MockService<T>() where T : class
+        {
+            return Substitute.For<T>();
+        }
+
+        protected Mock<T> MyMockService<T>() where T : class
+        {
+            if (typeof(T).IsInterface)
+            {
+                return new Mock<T>();
+            }
+            else
+            {
+                throw new InvalidOperationException("You should register from an interface");
+            }
+        }
+    }
+}
