86f07fddaae921dcc543e8237c8777d575e5cef7
enes.ozturk.de@gmail.com <enes.ozturk.de@gmail.com>
2021-04-02 15:43:26 +0200

    has removed unused parameter

--- CCC.Data.Monizoring/Startup.cs
+++ CCC.Data.Monizoring/Startup.cs
@@ -42,7 +42,7 @@ namespace CCC.Data.Monitoring
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, MonitoringDbContext context, IAccountRepository accountRepository,
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IAccountRepository accountRepository,
                                 IMonitorDataRepository monitorDataRepository, IQueueGroupRepository queueGroupRepository)
         {
             if (env.IsDevelopment())
